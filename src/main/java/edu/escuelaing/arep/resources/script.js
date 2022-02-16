5aa3bb2a5d9d0868edcc2d92be0b3e220187600d
Juan Sebastian Cadavid <juan.cadavid-p@mail.escuelaing.edu.co>
2022-02-15 20:52:52 -0500

    Mejora frontend

--- src/main/java/edu/escuelaing/arep/resources/script.js
+++ src/main/java/edu/escuelaing/arep/resources/script.js
@@ -2,6 +2,8 @@ let numberc;
 let numberf;
 let res1;
 let res2;
+let res = "";
+
 
 
 function inputValues() {
@@ -15,22 +17,34 @@ function inputValues() {
     const url1 = `${url}/celsiusF?value=${numberc}`;
     const url2 = `${url}/fahrenheitC?value=${numberf}`;
 
-    if(numberc != "") {
-        getapi(url1, "C");
-    }
-    if (numberf != "") {
-        getapi(url2, "F");
-    }
-    console.log(res1.result, res2.result);
+    getapi(url1, url2, "C", "F");
 }
 
-async function getapi (url, s) {
-    try {
-        const response = await fetch(url, {method: 'GET', headers: {'Content-Type': 'application/json'}});
-        let data = await response.json();
-        alert(`su resultado en ${s} es ${data.result}`);
+async function getapi (url1, url2, s, c) {
+    res1 = "";
+    res2 = "";
 
-    }catch (e) {
-        console.log(e);
+    if(numberc != "") {
+        try {
+            const response = await fetch(url1, {method: 'GET', headers: {'Content-Type': 'application/json'}});
+            let data = await response.json();
+            res1 = `su resultado en ${s} es ${data.result}`;
+
+        }catch (e) {
+            console.log(e);
+        }
     }
+    if (numberf != "") {
+        try {
+            const response = await fetch(url2, {method: 'GET', headers: {'Content-Type': 'application/json'}});
+            let data = await response.json();
+            res2 = `su resultado en ${c} es ${data.result}`;
+
+        }catch (e) {
+            console.log(e);
+        }
+    }
+    console.log(res1.result, res2.result);
+    var x = document.getElementById("resultt");
+    x.querySelector(".example").innerHTML = (res1 + "<br/>" + res2);
 }
